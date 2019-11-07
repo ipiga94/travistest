ed966bef9a9a08e01a65d7943e462f6f7fc53486
Ilaria Pigazzini <ipiga94@gmail.com>
2019-11-07 18:02:41 +0100

    first commit with travis.yml

---
+++ travistest/src/main/java/ilaria/travistest/App.java
@@ -0,0 +1,17 @@
+package ilaria.travistest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	if(args.length != 0) {
+    		System.out.println( "Hello " + args[0] ); 		
+    	} else {
+    		System.out.println( "What's your name????");
+    	}
+    }
+}
